69ab31d9ea3dfd1231f4c097b98c02591a331c55
Othavio-web <66527646+Othavio-web@users.noreply.github.com>
2024-04-27 23:38:50 -0300

    Add files via upload

---
+++ cursoJDevTreinamento/cursoJDevTreinamento/src/main/java/br/com/springboot/cursoJDevTreinamento/controllers/GreetingsController.java
@@ -0,0 +1,33 @@
+package br.com.springboot.cursoJDevTreinamento.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * A sample greetings controller to return greeting text
+ */
+@RestController
+public class GreetingsController {
+    /**
+     *
+     * @param name the name to greet
+     * @return greeting text
+     */
+    @GetMapping("/{name}")
+    @ResponseStatus(HttpStatus.OK)
+    public String greetingText(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+    
+    @RequestMapping(value = "/olaMundo/{nome}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String metodoRetorno2(@PathVariable String nome) {
+    	return "Olá mundo "+nome;
+    }
+}
